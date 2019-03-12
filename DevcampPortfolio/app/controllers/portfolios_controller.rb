f3ccbe96b8d4d093ac8d8a33ab8cb2744b6795c7
sebaslh12 <sebaslh12@gmail.com>
2019-03-11 20:26:12 -0500

    Implemented custom scopes for portfolio

--- DevcampPortfolio/app/controllers/portfolios_controller.rb
+++ DevcampPortfolio/app/controllers/portfolios_controller.rb
@@ -1,6 +1,10 @@
 class PortfoliosController < ApplicationController
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.ruby_on_rails
+  end
+
+  def angular 
+    @angular_portfolio_items = Portfolio.angular
   end
 
   def new
--- DevcampPortfolio/db/seeds.rb
+++ DevcampPortfolio/db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "3 topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium omnis rem dolore eligendi optio dolor pariatur! Fugit, quis! Aut ut, cum dignissimos nihil illum eius repudiandae libero sit officiis neque.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,13 +25,24 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title #{portfolio_item}",
-    subtitle: "My great service",
+    subtitle: "Ruby on rails",
     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium omnis rem dolore eligendi optio dolor pariatur! Fugit, quis! Aut ut, cum dignissimos nihil illum eius repudiandae libero sit officiis neque.",
     main_image: "http://placehold.it/600x400",
     thumb_image: "http://placehold.it/350x200",
   )
 end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title #{portfolio_item}",
+    subtitle: "Angular",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium omnis rem dolore eligendi optio dolor pariatur! Fugit, quis! Aut ut, cum dignissimos nihil illum eius repudiandae libero sit officiis neque.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200",
+  )
+end
+
 puts "9 portfolio items created"
